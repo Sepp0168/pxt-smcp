917fbe6eec3589f01df4dd41409ec5db08458186
Sepp0168 <sepp.programming11@outlook.com>
2025-07-03 11:19:48 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -333,16 +333,17 @@ namespace SMCP {
      * When initialise is not run, it will display error number 1
      * @param ReqPry if it should be first sender (1), first receiver (0) or none (-1)
      */
-    //% blockId="connect" block="connect to other microbit|| this device must be $ReqPry" blockExternalInputs=true
+    //% blockId="connect" block="connect to other microbit|| connect from far away $FarConnectMode this device must $ReqPry" icon = "\uf080" blockExternalInputs=true
     //% ReqPry.defl=-1
     //% ReqPry.min=-1 ReqPry.max=1
+    //% FarConnectMode.shadow=toggleOnOff
     //% group="first steps"
-    export function connect(ReqPry?: CommunicationPriorityTypes) {
+    export function connect(FarConnectMode:boolean, ReqPry?: CommunicationPriorityTypes) {
         if (!Started) {
             basic.showNumber(1)
             return
         }
-        radio.setTransmitPower(1)
+        radio.setTransmitPower(FarConnectMode ? 1 : 7)
         ReqPryVar = (isNaN(ReqPry) ? -1 : ReqPry)
         while (Connected == 0 && ConnectingStage == 0) {
             flashstorage.remove("Disconnected")
