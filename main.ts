ee4238f27a29fd58a048b5d6e1c4730c0f40e95b
Sepp0168 <sepp.programming11@outlook.com>
2025-07-03 15:29:41 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -206,6 +206,7 @@ namespace SMCP {
     //% blockId="init" block="Send number $int to other microbit"
     //% int.defl=1
     //% int.min=-99999999 number.max=99999999
+    //% group="messages"
     export function sendInt(int:number) {
         radio.sendNumber(int)
     }
